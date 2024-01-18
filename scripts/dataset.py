8359875f4b4b32f1e2024f0dfe9b4a0f5c07a23e
JSGrondin <js_grondin@hotmail.com>
2024-01-18 14:42:52 -0500

    updated dataset class

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -1,34 +1,73 @@
 # Your Imports
+import pandas as pd
+import nltk
+import string
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from sklearn.model_selection import train_test_split
 
-
-# For all methods in this class, feel free to change (remove/add) the parameters if needed
 class Dataset:
-    # TODO :: If necessary, add variables that you need to initiate this class
-    def __init__(self, filename):
+    def __init__(self, filename, stopwords_path):
         self.filename = filename
+        self.stopwords_path = stopwords_path
 
         self._load_data()
+        self._load_stopwords()
         self._pre_process()
 
-    # TODO :: Load the dataset
     def _load_data(self):
         print("Loading the data")
+        self.df = pd.read_csv(self.filename)
+        print(f"Dataset has {len(self.df)} rows.")
+        print(f"Dataset has the following columns: {self.df.columns}")
+        nans_in_df = {col: self.df[col].isnull().sum() for col in self.df.columns}
+        print(f"Dataset has the following count of NaNs: {nans_in_df}")
 
-    # TODO :: Complete this method that applies the pre-process steps on the dataset (4 steps maximum).
+    def _load_stopwords(self):
+        with open(self.stopwords_path, "r") as f:
+            stopwords = f.read()
+            stopwords = stopwords.split('\n')[:-1]
+        self.stopwords = stopwords
+
     def _pre_process(self):
         print("Pre-processing the text data...")
+        self.df['Abstract'] = self.df['Abstract'].apply(lambda x: self._process_text(x, self.stopwords))
 
-    # TODO :: create a training and validation split of the dataset
+    @staticmethod
+    def _process_text(text, stopwords):
+        """
+        Processes the input text by performing the following steps:
+        1. Tokenizing text
+        2. Removing stopwords
+        3. Removing punctuation
+        4. Lemmatizing remaining tokens
+        """
+        tokens = word_tokenize(text)
+        punctuation = set(string.punctuation)
+        stemmer = PorterStemmer()
+
+        processed_text = []
+        for word in tokens:
+            lower_word = word.lower()
+            if lower_word not in stopwords and lower_word not in punctuation:
+                stemmed = stemmer.stem(lower_word)
+                processed_text.append(stemmed)
+        
+        return ' '.join(processed_text)    
+    
     def create_splits(self):
         print("Creating splits")
-        return ...
+        # Split the data into training and validation sets (80% train, 20% validation)
+        X_train, X_val, y_train, y_val = train_test_split(self.text_data, self.labels, test_size=0.2, random_state=42)
+        return X_train, X_val, y_train, y_val
 
-    # TODO :: Return the pre-processed text data from all samples in the dataset
     @property
     def text_data(self):
-        return ...
+        return self.df['Abstract']
 
-    # TODO :: Return the labels from all samples in the dataset
     @property
     def labels(self):
-        return ...
+        return self.df['Category']
--- scripts/main_loop.py
+++ scripts/main_loop.py
@@ -1,7 +1,19 @@
 # Your imports
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+
+from dataset import Dataset
 
 if __name__ == '__main__':
 
-    data = "../dataset/abstract_arxiv.csv"
+    data_path = "../dataset/abstract_arxiv.csv"
+    stopwords_path = "../utils/stopwords.txt"
+
+    # Create Dataset object
+    data = Dataset(data_path, stopwords_path)
+
+    # Create splits
+    X_train, X_val, y_train, y_val = data.create_splits()
 
     # TODO :: Complete the main (refer to the assignment.md or devoir.md)
